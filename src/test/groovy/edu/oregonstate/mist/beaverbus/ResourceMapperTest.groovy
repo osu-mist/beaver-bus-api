83829d0a0ed6ab9343fb46a4c8cdd57dc9274b46
Andrew Ekstedt <ekstedta@oregonstate.edu>
2018-01-12 14:34:18 -0800

    add a ResourceMapper test
    CO-998
    
    Add a simple test for ResourceMapper.mapRoute that just checks the
    result on a hardcoded input. This doesn't test quite as much as i would
    like: the input to mapRoute is a java object; i would like to also test
    that the json response from RideSystems is parsed correctly, but that's
    a little harder.

---
+++ src/test/groovy/edu/oregonstate/mist/beaverbus/ResourceMapperTest.groovy
@@ -0,0 +1,81 @@
+import edu.oregonstate.mist.api.jsonapi.ResourceObject
+import edu.oregonstate.mist.beaverbus.BeaverBusUriBuilder
+import edu.oregonstate.mist.beaverbus.ResourceMapper
+import edu.oregonstate.mist.beaverbus.RouteStop
+import edu.oregonstate.mist.beaverbus.RouteWithSchedule
+import edu.oregonstate.mist.beaverbus.core.RouteAttributes
+import edu.oregonstate.mist.beaverbus.core.Stop
+import groovy.transform.CompileStatic
+import org.junit.Assert
+import org.junit.Test
+
+@CompileStatic
+class ResourceMapperTest {
+    ResourceMapper mapper = new ResourceMapper()
+    BeaverBusUriBuilder uriBuilder = new BeaverBusUriBuilder(new URI("http://example.com/"))
+
+    @Test
+    void testMapRoute() {
+        def input = new RouteWithSchedule(
+                "Description": "West Route 2",
+                "EncodedPolyline": "q__oGjlmoV?a@?eC?i@?mA?Y??V@`@AV?HAD?FAJCHC~@Y??NGTKDAFADAH?LAhA?|@?j@?d@?H?@?B?D@F@NBJ?^?^???nC?LD???xA?`F?fABT?N??@FBRBRDRFTDR??HTDJHJFH???@HHf@d@HHJFHDJDJBNBL@B?V@N?FAFAHAFEJIFIHMfAeBb@m@DEDCFAPC??FYBS???CBS@U@O?Q?O?OAM?OAOCQAOAKAIAG??Z?`@Ad@?`@?D?B?@AB?BABAZS??\\~@XbAJb@Np@Hh@Df@Db@BPBb@@JB^@\\?v@AnCCnC??}@@i@AM???sB?aB?gBAwC?m@Ae@?w@?_AA}@?K?cC?A?I?C?KOCCAAECEAEAICG?O?i@?kAAeB?O???wE????gB?gA?gA?g@?m@?sF",
+                "MapLatitude": 44.560947 as Double,
+                "MapLineColor": "#8c702d",
+                "MapLongitude": -123.282271 as Double,
+                "MapZoom": 15,
+                "RouteID": 19,
+                "Stops": [
+                        new RouteStop(
+                                "Latitude": 44.5645297213 as Double,
+                                "Longitude": -123.2815022394 as Double,
+                                "Description": "Buxton Hall",
+                                "Order": 1,
+                                "RouteID": 19,
+                                "RouteStopID": 210,
+                        ),
+                        new RouteStop(
+                                "Latitude": 44.5636029495 as Double,
+                                "Longitude": -123.2797954233 as Double,
+                                "Description": "Weatherford Hall",
+                                "Order": 2,
+                                "RouteID": 19,
+                                "RouteStopID": 211,
+                        )
+                ]
+        )
+
+        def expected = new ResourceObject(
+                "id": "19",
+                "type": "route",
+                "attributes": new RouteAttributes(
+                        "description": "West Route 2",
+                        "encodedPolyline": "q__oGjlmoV?a@?eC?i@?mA?Y??V@`@AV?HAD?FAJCHC~@Y??NGTKDAFADAH?LAhA?|@?j@?d@?H?@?B?D@F@NBJ?^?^???nC?LD???xA?`F?fABT?N??@FBRBRDRFTDR??HTDJHJFH???@HHf@d@HHJFHDJDJBNBL@B?V@N?FAFAHAFEJIFIHMfAeBb@m@DEDCFAPC??FYBS???CBS@U@O?Q?O?OAM?OAOCQAOAKAIAG??Z?`@Ad@?`@?D?B?@AB?BABAZS??\\~@XbAJb@Np@Hh@Df@Db@BPBb@@JB^@\\?v@AnCCnC??}@@i@AM???sB?aB?gBAwC?m@Ae@?w@?_AA}@?K?cC?A?I?C?KOCCAAECEAEAICG?O?i@?kAAeB?O???wE????gB?gA?gA?g@?m@?sF",
+                        "mapColor": "#8c702d",
+                        "latitude": 44.560947 as Double,
+                        "longitude": -123.282271 as Double,
+                        "zoomLevel": 15,
+                        "stops": [
+                                new Stop(
+                                        "stopID": "210",
+                                        "description": "Buxton Hall",
+                                        "latitude": 44.5645297213 as Double,
+                                        "longitude": -123.2815022394 as Double,
+                                ),
+                                new Stop(
+                                        "stopID": "211",
+                                        "description": "Weatherford Hall",
+                                        "latitude": 44.5636029495 as Double,
+                                        "longitude": -123.2797954233 as Double,
+                                ),
+                        ]
+                ),
+                "links": [
+                        "self": new URI("http://example.com/routes/19"),
+                ]
+        )
+
+
+        def actual = mapper.mapRoute(input, uriBuilder)
+        assert actual == expected
+    }
+}
