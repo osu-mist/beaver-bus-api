cffaf436d7abe137d1fa2a258ed5711faa35bf3c
Andrew Ekstedt <ekstedta@oregonstate.edu>
2018-01-19 15:07:19 -0800

    include /beaverbus in generate URLs
    CO-998

--- src/main/groovy/edu/oregonstate/mist/beaverbus/BeaverBusUriBuilder.groovy
+++ src/main/groovy/edu/oregonstate/mist/beaverbus/BeaverBusUriBuilder.groovy
@@ -11,19 +11,19 @@ class BeaverBusUriBuilder {
 
     URI routeUri(Integer id) {
         UriBuilder.fromUri(this.endpointUri)
-                .path("routes/{id}")
+                .path("beaverbus/routes/{id}")
                 .build(id)
     }
 
     URI vehicleUri(Integer id) {
         UriBuilder.fromUri(this.endpointUri)
-                .path("vehicles/{id}")
+                .path("beaverbus/vehicles/{id}")
                 .build(id)
     }
 
     URI arrivalUri(Integer routeID, Integer stopID) {
         UriBuilder.fromUri(endpointUri)
-                .path("arrivals")
+                .path("beaverbus/arrivals")
                 .queryParam("routeID", "{routeID}")
                 .queryParam("stopID", "{stopID}")
                 .build(routeID, stopID)
